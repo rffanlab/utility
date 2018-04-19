934d77f996a1afd7c6e97616b4dc972274883a96
rffanlab <admin@rffan.com>
2018-04-19 15:44:58 +0800

    将时间方法移动到同一个文件中。

--- opTime.go
+++ opTime.go
@@ -4,6 +4,7 @@ import (
 	"time"
 	"fmt"
 	"strings"
+	"strconv"
 )
 
 // 防止忘记
@@ -96,6 +97,37 @@ func TimeNowForSecond() string {
 
 func TimeToTimestamp(date time.Time) int64 {
 	return date.Unix()
+}
+
+
+func Year() string {
+	return strconv.Itoa(time.Now().Year())
+}
+
+func Month() string {
+	theMonth := int(time.Now().Month())
+	var month string
+	if theMonth <10 {
+		month = fmt.Sprintf("0%d",theMonth)
+	} else {
+		month = fmt.Sprintf("%d",theMonth)
+	}
+	return month
+}
+
+func Day() string {
+	theDay := time.Now().Day()
+	var returnDay string
+	if theDay<10 {
+		returnDay = fmt.Sprintf("0%d",theDay)
+	}else {
+		returnDay = fmt.Sprintf("%d",theDay)
+	}
+	return returnDay
+}
+
+func Today() string {
+	return fmt.Sprintf("%s_%s_%s",Year(),Month(),Day())
 }
 
 
