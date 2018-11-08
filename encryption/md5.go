09d174bcae6cd19cdbf64500799ef7fbbf7e2f00
rffanlab <admin@rffan.com>
2018-11-08 09:42:23 +0800

    添加了各类加密方法

--- encryption/md5.go
+++ encryption/md5.go
@@ -3,6 +3,7 @@ package encryption
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"github.com/pkg/errors"
 	"math/rand"
 	"time"
 )
@@ -43,4 +44,44 @@ func RandStr(length int) string {
 		result = append(result, bytes[rand.Intn(len(bytes))])
 	}
 	return string(result)
+}
+
+// 方法：盐值加密(盐值长度为固定4个字符串)
+/*
+*  传入参数：
+*  @Param:strToEncrypt Type:string
+*  @Param: Type:
+*  @Param: Type:
+*  返回参数：
+*  @Param: Type:string
+*  @Param: Type:
+*/
+func EncryptStrWithSalt(strToEncrypt string) string {
+	salt := RandStr(4)
+	logStr := strToEncrypt + salt
+	encryptedStr := Md5(logStr)
+	return encryptedStr+salt
+}
+
+// 方法：传入的字符串与盐值加密后的字符串的对比
+/*
+*  传入参数：
+*  @Param:strToCompare Type:string
+*  @Param:encryptStr Type:string
+*  @Param: Type:
+*  返回参数：
+*  @Param:bool Type:bool
+*  @Param:error Type:error
+*/
+func CompareStrToSaltEncryptedStr(strToCompare, encryptedStr string) (bool,error) {
+	if len(encryptedStr) != 36 {
+		return false,errors.New("Not A Vaild String To Compare")
+	}
+	salt := string([]rune(encryptedStr)[32:36])
+	newEncrypt := Md5(strToCompare+salt)
+	if newEncrypt+salt == encryptedStr{
+		return true,nil
+	}else {
+		return false,nil
+	}
 }
