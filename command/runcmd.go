859b019fac27faa023b0d3861f091a343ecc756c
rffanlab <admin@rffan.com>
2019-08-28 09:41:30 +0800

    修复了命令运行的问题，将命令运行添加了错误的输出。

--- command/runcmd.go
+++ command/runcmd.go
@@ -6,16 +6,18 @@ import (
 	"os/exec"
 )
 
-func RunCmd(theCmd string, args ...string) (out string,err error) {
+func RunCmd(theCmd string, args ...string) (out,errout string,err error) {
 	cmd := exec.Command(theCmd, args...)
 	// 获取输出对象，可以从该对象中读取输出结果
 	stdout, err := cmd.StdoutPipe()
+	stderr,err := cmd.StderrPipe()
 	if err != nil {
 		logs.Error(err)
 		return
 	}
 	// 保证关闭输出流
 	defer stdout.Close()
+	defer stderr.Close()
 	// 运行命令
 	if err = cmd.Start(); err != nil {
 		logs.Error(err)
@@ -25,7 +27,14 @@ func RunCmd(theCmd string, args ...string) (out string,err error) {
 	opBytes, err := ioutil.ReadAll(stdout)
 	if err != nil {
 		logs.Error(err)
+		return
 	}
 	out = string(opBytes)
+	opBytes,err = ioutil.ReadAll(stderr)
+	if err != nil {
+		logs.Error(err)
+		return
+	}
+	errout = string(opBytes)
 	return
 }
